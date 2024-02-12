d39616982cfaeb7a4614fd78969457cbf42bdc2e
John T. Foster <johntfosterjr@gmail.com>
2024-02-12 10:13:31 -0600

    First commit

--- test.py
+++ test.py
@@ -42,14 +42,6 @@ class TestSolution(unittest.TestCase):
                                              0.00194181, 0.00199742, 0.0022314, 
                                              0.00205417]), atol=0.0001)
 
-    def test_transform_private(self):
-
-        kc = KozenyCarmen('poro_perm.csv')
-
-        np.testing.assert_allclose(kc.kc_model()[22:31], 
-                                   np.array([0.00328828, 0.00183395, 0.00290263, 0.00241945,
-                                             0.00211207, 0.00229286, 0.00144518, 0.00173048, 
-                                             0.00217115]), atol=0.0001)
 
 if __name__ == '__main__':
     unittest.main()
