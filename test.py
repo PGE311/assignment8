86ade24efdf6fe25d26b042b52f92fdc473a2be1
John T. Foster <john.foster@utexas.edu>
2026-02-11 12:35:26 -0600

    Update for 2026

--- test.py
+++ test.py
@@ -23,10 +23,8 @@ with open("assignment8.ipynb") as f:
     exporter = nbconvert.PythonExporter()
     python_file, _ = exporter.from_file(f)
 
-
 with open("assignment8.py", "w") as f:
     f.write(python_file)
-
 
 from assignment8 import KozenyCarmen
 
@@ -41,15 +39,6 @@ class TestSolution(unittest.TestCase):
                                              0.00073352, 0.0035369, 0.00123457, 
                                              0.00194181, 0.00199742, 0.0022314, 
                                              0.00205417]), atol=0.0001)
-
-    def test_transform_private(self):
-
-        kc = KozenyCarmen('poro_perm.csv')
-
-        np.testing.assert_allclose(kc.kc_model()[22:31], 
-                                   np.array([0.00328828, 0.00183395, 0.00290263, 0.00241945,
-                                             0.00211207, 0.00229286, 0.00144518, 0.00173048, 
-                                             0.00217115]), atol=0.0001)
 
 if __name__ == '__main__':
     unittest.main()
